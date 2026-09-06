2779590d56a1f23a93833d0978598e515a23e178
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-06 22:07:01 +0530

    sum

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,7 +3,6 @@ import { inngest } from "../../../lib/client";
 import { checkBudgetAlert, generateMonthlyReports,triggerRecurringTransactions,
     processRecurringTransaction } from "../../../lib/function";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
