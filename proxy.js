122cf2e03e6152f089bbe3a1d0e7c49fe48ec30d
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-07 01:01:03 +0530

    Fix Clerk proxy

--- proxy.js
+++ proxy.js
@@ -1,10 +1,63 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import arcjet, {
+  detectBot,
+  shield,
+} from "@arcjet/next";
 
-export default clerkMiddleware({
-  frontendApiProxy: {
-    enabled: true,
-  },
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/secure(.*)",
+  "/dashboard(.*)",
+  "/transaction(.*)",
+  "/account(.*)",
+]);
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "GO_HTTP",
+      ],
+    }),
+  ],
 });
+
+export default clerkMiddleware(
+  async (auth, req) => {
+    // Don't let Arcjet interfere with Clerk's Frontend API proxy.
+    if (!req.nextUrl.pathname.startsWith("/__clerk")) {
+      const decision = await aj.protect(req);
+
+      if (decision.isDenied()) {
+        return new Response("Forbidden", {
+          status: 403,
+        });
+      }
+    }
+
+    const { userId, redirectToSignIn } = await auth();
+
+    if (isProtectedRoute(req) && !userId) {
+      return redirectToSignIn();
+    }
+  },
+  {
+    frontendApiProxy: {
+      enabled: true,
+    },
+  }
+);
 
 export const config = {
   matcher: [
