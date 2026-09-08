f3c801cf1fe796a67871c693da2557ccf97d8cb4
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-08 12:55:01 +0530

    Test Clerk frontend API proxy

--- proxy.js
+++ proxy.js
@@ -1,15 +1,70 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import arcjet, {
+  detectBot,
+  shield,
+} from "@arcjet/next";
 
-export default clerkMiddleware({
-  frontendApiProxy: {
-    enabled: true,
-  },
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+} from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/secure/:path*",
+  "/dashboard/:path*",
+  "/transaction/:path*",
+  "/account/:path*",
+]);
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "GO_HTTP",
+      ],
+    }),
+  ],
 });
+
+export default clerkMiddleware(
+  async (auth, req) => {
+    // Do NOT run Arcjet on Clerk's Frontend API proxy.
+    if (!req.nextUrl.pathname.startsWith("/__clerk")) {
+      const decision = await aj.protect(req);
+
+      if (decision.isDenied()) {
+        return new Response("Forbidden", {
+          status: 403,
+        });
+      }
+    }
+
+    const { userId, redirectToSignIn } = await auth();
+
+    if (isProtectedRoute(req) && !userId) {
+      return redirectToSignIn();
+    }
+  },
+  {
+    frontendApiProxy: {
+      enabled: true,
+    },
+  }
+);
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
     "/(api|trpc)(.*)",
+
     "/__clerk/(.*)",
   ],
 };
