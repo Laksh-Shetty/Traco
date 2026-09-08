0c68f252a063491c3916fba0e33055615c714f75
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-08 11:00:25 +0530

    new

--- proxy.js
+++ proxy.js
@@ -9,10 +9,10 @@ import {
 } from "@clerk/nextjs/server";
 
 const isProtectedRoute = createRouteMatcher([
-  "/secure(.*)",
-  "/dashboard(.*)",
-  "/transaction(.*)",
-  "/account(.*)",
+  "/secure/:path*",
+  "/dashboard/:path*",
+  "/transaction/:path*",
+  "/account/:path*",
 ]);
 
 const aj = arcjet({
@@ -33,29 +33,38 @@ const aj = arcjet({
   ],
 });
 
-export default clerkMiddleware(async (auth, req) => {
-  // Don't let Arcjet interfere with Clerk's proxy path.
-  if (!req.nextUrl.pathname.startsWith("/__clerk")) {
-    const decision = await aj.protect(req);
+export default clerkMiddleware(
+  async (auth, req) => {
+    // Do NOT run Arcjet on Clerk's Frontend API proxy.
+    if (!req.nextUrl.pathname.startsWith("/__clerk")) {
+      const decision = await aj.protect(req);
 
-    if (decision.isDenied()) {
-      return new Response("Forbidden", {
-        status: 403,
-      });
+      if (decision.isDenied()) {
+        return new Response("Forbidden", {
+          status: 403,
+        });
+      }
     }
-  }
 
-  const { userId, redirectToSignIn } = await auth();
+    const { userId, redirectToSignIn } = await auth();
 
-  if (isProtectedRoute(req) && !userId) {
-    return redirectToSignIn();
+    if (isProtectedRoute(req) && !userId) {
+      return redirectToSignIn();
+    }
+  },
+  {
+    frontendApiProxy: {
+      enabled: true,
+    },
   }
-});
+);
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+
     "/(api|trpc)(.*)",
-    "/__clerk/:path*",
+
+    "/__clerk/(.*)",
   ],
 };
