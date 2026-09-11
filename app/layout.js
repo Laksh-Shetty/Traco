43fe806df5ac67c6850451cd3dde86f226d50f7b
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-11 15:07:49 +0530

    masti

--- app/layout.js
+++ app/layout.js
@@ -27,7 +27,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider proxyUrl="https://traco-six.vercel.app/__clerk">
       <html lang="en">
         <body className={`${inter.className} flex flex-col min-h-screen`}>
           <Header />
