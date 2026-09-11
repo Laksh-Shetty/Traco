d091e6ba265349d1c865d6b8c820e8d738f6395e
Laksh-Shetty <lakshshetty22@gmail.com>
2026-09-11 16:31:36 +0530

    push

--- proxy.js
+++ proxy.js
@@ -33,38 +33,26 @@ const aj = arcjet({
   ],
 });
 
-export default clerkMiddleware(
-  async (auth, req) => {
-    // Do NOT run Arcjet on Clerk's Frontend API proxy.
-    if (!req.nextUrl.pathname.startsWith("/__clerk")) {
-      const decision = await aj.protect(req);
+export default clerkMiddleware(async (auth, req) => {
+  const decision = await aj.protect(req);
 
-      if (decision.isDenied()) {
-        return new Response("Forbidden", {
-          status: 403,
-        });
-      }
-    }
-
-    const { userId, redirectToSignIn } = await auth();
-
-    if (isProtectedRoute(req) && !userId) {
-      return redirectToSignIn();
-    }
-  },
-  {
-    frontendApiProxy: {
-      enabled: true,
-    },
+  if (decision.isDenied()) {
+    return new Response("Forbidden", {
+      status: 403,
+    });
   }
-);
+
+  const { userId, redirectToSignIn } = await auth();
+
+  if (isProtectedRoute(req) && !userId) {
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
 
     "/(api|trpc)(.*)",
-
-    "/__clerk/(.*)",
   ],
 };
